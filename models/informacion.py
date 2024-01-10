84559a429c08125698bf80290d056d232a209171
a22javiermm <javiermatomontoto@gmail.com>
2024-01-10 13:51:54 +0100

    commit 8

--- models/informacion.py
+++ models/informacion.py
@@ -3,6 +3,7 @@
 from odoo import models, fields, api
 from odoo.exceptions import ValidationError
 
+
 class informacion(models.Model):
     _name = 'odoo_basico.informacion'
     _description = 'Exemplo informacion'
@@ -14,17 +15,30 @@ class informacion(models.Model):
     alto_en_cm = fields.Integer(string="Alto en centímetros:")
     longo_en_cm = fields.Integer(string="Longo en centímetros:")
     ancho_en_cm = fields.Integer(string="Ancho en centímetros:")
-    volume = fields.Float(digits=(6,7), compute="_volume", store=True, string="Volume m3")
+    volume = fields.Float(digits=(6, 7), compute="_volume", store=True, string="Volume m3")
     literal = fields.Char(store=False)
-    peso = fields.Float(digits=(6,2), default = 2.7, string="Peso en KG.:")
+    peso = fields.Float(digits=(6, 2), default=2.7, string="Peso en KG.:")
     densidade = fields.Float(compute="_densidade", store=True, string="Densidade en KG/m3:")
     autorizado = fields.Boolean(default=True, string="¿Autorizado?")
-    sexo_traducido = fields.Selection([('Hombre','Home'),('Mujer','Muller'),('Otros','Outros')], string="Sexo:")
+    sexo_traducido = fields.Selection([('Hombre', 'Home'), ('Mujer', 'Muller'), ('Otros', 'Outros')], string="Sexo:")
+    foto = fields.Binary(string='Foto')
+    adxunto_nome = fields.Char(string="Nome Adxunto")
+    adxunto = fields.Binary(string="Arquivo adxunto")
+    # Os campos Many2one crean un campo na BD
+    moeda_id = fields.Many2one('res.currency', domain="[('position','=','after')]")
+    # con domain, filtramos os valores mostrados. Pode ser mediante unha constante (vai entre comillas) ou unha variable
+    moeda_euro_id = fields.Many2one('res.currency',
+                                    default=lambda self: self.env['res.currency'].search([('name', '=', "EUR")],
+                                    limit=1))
+    gasto_en_euros = fields.Monetary("Gasto en Euros", 'moeda_euro_id')
+    moeda_en_texto = fields.Char(related="moeda_id.currency_unit_label", string="Moeda en formato texto", store=True)
+    creador_da_moeda = fields.Char(related="moeda_id.create_uid.login", string="Usuario creador da moeda", store=True)
 
     @api.depends('alto_en_cm', 'longo_en_cm', 'ancho_en_cm')
     def _volume(self):
         for rexistro in self:
-            rexistro.volume = (float(rexistro.alto_en_cm) * float(rexistro.longo_en_cm) * float(rexistro.ancho_en_cm))/1000000
+            rexistro.volume = (float(rexistro.alto_en_cm) * float(rexistro.longo_en_cm) * float(
+                rexistro.ancho_en_cm)) / 1000000
 
     @api.depends('volume', 'peso')
     def _densidade(self):
